3886e673e6561fc253d3cd2ac8bbc64c644e45df
Joshua Montgomery <130615967+jmontgomery30@users.noreply.github.com>
2024-10-25 08:48:38 -0600

    Add `run.bat` file to automate the execution of the main program
    * **Change directory**
      - Change directory to `/Volumes/X10_Pro/AI_CLIPING/workspace-blank`
    
    * **Activate virtual environment**
      - Activate the virtual environment
    
    * **Run main program**
      - Run `main.py`

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Change directory to the location of main.py
+cd /Volumes/X10_Pro/AI_CLIPING/workspace-blank
+
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Run the main.py program
+python main.py
